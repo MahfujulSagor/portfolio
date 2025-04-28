bd746e9f0387737cdf5b878dfc3dbcc753c2c02f
MahfujulSagor <me24020@mbstu.ac.bd>
2025-04-28 09:59:10 +0600

    add animation to hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,15 +1,41 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useEffect, useLayoutEffect, useRef } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
-  const scrollRef1 = useRef(null);
-  const scrollRef2 = useRef(null);
+  const scrollRef1 = useRef<HTMLHeadingElement | null>(null);
+  const scrollRef2 = useRef<HTMLHeadingElement | null>(null);
+  const info1 = useRef<HTMLDivElement | null>(null);
+  const info2 = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
     if (!scrollRef1 || !scrollRef2) return;
+
+    gsap.fromTo(
+      scrollRef1.current,
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        duration: 0.5,
+        ease: "power1.inOut",
+      }
+    );
+
+    gsap.fromTo(
+      scrollRef2.current,
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        duration: 0.5,
+        ease: "power1.inOut",
+      }
+    );
 
     gsap.to(scrollRef1.current, {
       x: -250,
@@ -32,23 +58,67 @@ const Hero = () => {
       },
     });
   }, []);
+
+  useLayoutEffect(() => {
+    if (!info1.current || !info2.current) return;
+
+    gsap.fromTo(
+      info1.current,
+      {
+        opacity: 0,
+        x: 100,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 0.5,
+        ease: "power1.inOut",
+      }
+    );
+
+    gsap.fromTo(
+      info2.current,
+      {
+        opacity: 0,
+        x: -100,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 0.5,
+        ease: "power1.inOut",
+      }
+    );
+  });
   return (
     <section className="max-w-[1200px] overflow-hidden w-full min-h-screen flex justify-center items-center">
       <div className="w-full">
         <div className="flex justify-start gap-28 items-center w-full">
-          <div>
+          <div className="w-full flex gap-6 justify-start items-center">
             <h1
               ref={scrollRef1}
-              className="text-[150px] tracking-tight font-extrabold leading-none"
+              className="text-[150px] tracking-tighter font-extrabold leading-none"
             >
               Mahfujul
             </h1>
+            <div ref={info1} id="info1" className="">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Necessitatibus repellat nobis laboriosam itaque, sequi facilis.
+              </p>
+            </div>
           </div>
         </div>
         <div className="w-full flex justify-end items-center">
+          <div ref={info2} id="info2">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+              consectetur maxime distinctio quod facere amet blanditiis labore.
+            </p>
+          </div>
           <h1
             ref={scrollRef2}
-            className="text-[150px] tracking-tight font-extrabold leading-none"
+            className="text-[150px] tracking-tighter font-extrabold leading-none"
           >
             Sagor
           </h1>
