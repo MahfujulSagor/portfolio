2b0257db18c8275e04f26b584a9b87872512a634
MahfujulSagor <me24020@mbstu.ac.bd>
2025-04-29 16:59:00 +0600

    make logo clickable

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,17 +1,22 @@
+"use client";
 import Image from "next/image";
 import React from "react";
 import logo from "@/public/logo.svg";
+import { handleLinkClick } from "@/utils/smoothScroll";
+import Link from "next/link";
 
 const Logo = () => {
   return (
-    <Image
-      src={logo}
-      width={100}
-      height={100}
-      loading="lazy"
-      alt="Mahfujul_Sagor"
-      className="object-cover hidden sm:flex"
-    />
+    <Link href={"#home"} onClick={(e) => handleLinkClick(e, "#home")}>
+      <Image
+        src={logo}
+        width={100}
+        height={100}
+        loading="lazy"
+        alt="Mahfujul_Sagor"
+        className="object-cover hidden sm:flex"
+      />
+    </Link>
   );
 };
 
